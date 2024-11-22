307332dbd8e8bb01db545db3f2aae4dfa243032b
Skyler <skylerwiernik@gmail.com>
2024-11-22 09:19:33 -0500

    Log button presses

--- robot.py
+++ robot.py
@@ -19,15 +19,19 @@ class Motor(Device):
     def __update_status_color(self, enabled: DeviceProperty):
         self.status_color.value = Color.SUCCESS if enabled.value else Color.DANGER
 
+    def __on_button(self):
+        print("Button pressed!")
+        self.dot.flash()
+
     def __make_device_cell(self, name):
-        dot = graphics.StatusDot(self.status_color, alignment=graphics.Alignment.RIGHT)
+        self.dot = graphics.StatusDot(self.status_color, alignment=graphics.Alignment.RIGHT)
         header = graphics.HeaderText(name)
         bodyText = graphics.BodyText("Motor", color=graphics.Color.SECONDARY)
         switch = graphics.Switch(managed_property=self.isEnabled)
-        button = graphics.Button("Flash", dot.flash, text_color=graphics.Color.hex("#f542a7"))
+        button = graphics.Button("Flash", self.__on_button, text_color=graphics.Color.hex("#f542a7"))
         slider = graphics.Slider(managed_property=self.speed, min_value=0., max_value=1., step=0.1)
         subText = graphics.SubText("Speed", color=graphics.Color.SECONDARY, alignment=Alignment.CENTER)
-        return graphics.GraphicCell([dot, header, bodyText, switch, button, slider, subText])
+        return graphics.GraphicCell([self.dot, header, bodyText, switch, button, slider, subText])
 
 
 
