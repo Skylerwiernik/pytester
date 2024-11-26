01a62a50ea120c38024ddc152850245a4219254d
Skyler <skylerwiernik@gmail.com>
2024-11-26 12:42:22 -0500

    Stripped main for example

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
-from robot import Ava
+from robot import ExampleRobot
 import asyncio
 async def main():
-    ava = Ava()
-    while True:
-        ava.motor1.speed.value = 0.5
-        ava.motor2.speed.value = -0.5
-        await asyncio.sleep(1)
-        ava.motor1.speed.value = -0.5
-        ava.motor2.speed.value = 0.5
-        await asyncio.sleep(1)
+    bot = ExampleRobot()
+
 
 if __name__ == "__main__":
     asyncio.run(main())
